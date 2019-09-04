6eb0c412e3fc9da2d224c1aaab1684f32ca7fe42
Anders Riutta <git@andersriutta.com>
2019-09-03 19:27:23 -0700

    Use latest jupyter launch code

--- shell-jupyter-launch.nix
+++ shell-jupyter-launch.nix
@@ -1,25 +1,28 @@
 { pkgs ? import <nixpkgs> {} }:
+with pkgs.lib.strings;
 let
   deps = import ./deps.nix;
+  shellHookBase = fileContents ./shell-hook.sh;
 in
   pkgs.mkShell {
     buildInputs = deps ++ [ pkgs.ps ];
-    shellHook = ''
-      bash ./shell-hook.sh
-
-      # If session is remote, don't open browser
-      # from https://unix.stackexchange.com/a/9607
-      if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-        SESSION_TYPE=remote/ssh
-      else
-        case $(ps -o comm= -p $PPID) in
-        sshd | */sshd) SESSION_TYPE=remote/ssh ;;
-        esac
-      fi
-      no_browser_flag=""
-      if [ "$SESSION_TYPE" == "remote/ssh" ]; then
-        no_browser_flag="--no-browser"
-      fi
-      eval "jupyter notebook $no_browser_flag --port=8888 --notebook-dir ./pymc3_hierarchical_tutorial"
-    '';
+    shellHook = concatStringsSep "\n" [
+      shellHookBase
+      ''
+        # If session is remote, don't open browser
+        # from https://unix.stackexchange.com/a/9607
+        if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+          SESSION_TYPE=remote/ssh
+        else
+          case $(ps -o comm= -p $PPID) in
+          sshd | */sshd) SESSION_TYPE=remote/ssh ;;
+          esac
+        fi
+        no_browser_flag=""
+        if [ "$SESSION_TYPE" == "remote/ssh" ]; then
+          no_browser_flag="--no-browser"
+        fi
+        eval "jupyter notebook $no_browser_flag --port=8888 --notebook-dir ./pymc3_hierarchical_tutorial"
+      ''
+    ];
 }
--- shell.nix
+++ shell.nix
@@ -1,13 +1,16 @@
 { pkgs ? import <nixpkgs> { config.allowUnfree = true; } }:
+with pkgs.lib.strings;
 let
   deps = import ./deps.nix;
+  shellHookBase = fileContents ./shell-hook.sh;
 in
   pkgs.mkShell {
     buildInputs = deps;
-    shellHook = ''
-      bash ./shell-hook.sh
-
-      # To open as a jupyter notebook:
-      # jupyter notebook --port=8888 --notebook-dir ./
-    '';
+    shellHook = concatStringsSep "\n" [
+      shellHookBase
+      ''
+        # To open as a jupyter notebook:
+        # jupyter notebook --port=8888 --notebook-dir ./pymc3_hierarchical_tutorial
+      ''
+    ];
 }
